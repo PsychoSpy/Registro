8908266f9c78e08616dd8cc9baac692ac3b0e683
User <User@DESKTOP-8GDV20H>
2024-02-12 18:43:03 -0300

    Adicionar arquivos de projeto.

---
+++ Registro/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Registro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.entrada = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rua = new System.Windows.Forms.Label();
+            this.numero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(88, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de endereços";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Entrada de endereço:";
+            // 
+            // entrada
+            // 
+            this.entrada.Location = new System.Drawing.Point(15, 72);
+            this.entrada.Name = "entrada";
+            this.entrada.Size = new System.Drawing.Size(231, 20);
+            this.entrada.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Endereço registrado:";
+            // 
+            // rua
+            // 
+            this.rua.AutoSize = true;
+            this.rua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.rua.Location = new System.Drawing.Point(12, 170);
+            this.rua.Name = "rua";
+            this.rua.Size = new System.Drawing.Size(36, 16);
+            this.rua.TabIndex = 5;
+            this.rua.Text = "Rua:";
+            // 
+            // numero
+            // 
+            this.numero.AutoSize = true;
+            this.numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.numero.Location = new System.Drawing.Point(12, 186);
+            this.numero.Name = "numero";
+            this.numero.Size = new System.Drawing.Size(59, 16);
+            this.numero.TabIndex = 6;
+            this.numero.Text = "Número:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 220);
+            this.Controls.Add(this.numero);
+            this.Controls.Add(this.rua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.entrada);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de Endereços";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox entrada;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label rua;
+        private System.Windows.Forms.Label numero;
+    }
+}
+
---
+++ Registro/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)// ao clicar em registro
+        {
+            string enderecoforn = this.Controls["entrada"].Text; // recebe a informação da caixa de texto
+            string indicador = "", complemento = "";
+            
+            // organiza e separa o endereço
+            string[] resultado = Classificacao(enderecoforn);
+            if(resultado[1] != null)
+            {
+                this.Controls["entrada"].Text = null; // apaga o texto digitado caso não existam erros
+            }
+            
+            this.Controls["rua"].Text = "Rua: " + resultado[0];
+            this.Controls["numero"].Text = "Número: " + resultado[1];
+        }
+        private string[] Classificacao(string analise)
+        {
+            string rua = "", numero = "";
+            
+            if(analise.Contains(" "))
+            {
+                string analiseSV;
+                if(analise.Contains(",")) // retira as virgulas pra ficar bonito 
+                {
+                    string[] semvirgula = analise.Split(',');
+                    analiseSV = string.Join(" ", semvirgula);
+                }
+                else
+                {
+                    analiseSV = analise;
+                }
+                string[] separa = analiseSV.Split(' ');
+                string indicador = null, complemento = null, numeroex = null;
+                List<string> palavras = new List<string>();
+                List<string> numeros = new List<string>();
+
+                if (Buscaindicador(separa) != null) // identifica indicadores de numeros
+                {
+                    indicador = Buscaindicador(separa);
+                }
+                if (Buscacomplemento(separa) != null) // identifica complementos de numeros
+                {
+                    complemento = Buscacomplemento(separa);
+                }
+                if(Buscanumerosexed(separa) != null)
+                {
+                    numeroex = Buscanumerosexed(separa);
+                }
+                foreach (var termo in separa) // separa numeros e palavras
+                {
+                    if (Contemnumero(termo))
+                    {
+                        if(termo == numeroex)
+                        {
+                            palavras.Add(termo);
+                        }
+                        else
+                        {
+                            numeros.Add(termo);
+                        }
+                    }
+                    else
+                    {
+                        if(indicador == null && complemento == null) // checa se há indicadores de numeros ou complementos
+                        {
+                            palavras.Add(termo);
+                        }
+                        else
+                        {
+                            if(termo != indicador) // checa se o termo analizado é um indicador, se for, é descartado
+                            {
+                                if(termo == complemento) // checa se o termo é um complemento, se for, é enviado à lista de numeros
+                                {
+                                    numeros.Add(termo);
+                                }
+                                else
+                                {
+                                    palavras.Add(termo);
+                                }
+                            }
+                        }
+                    }
+                }
+                if (palavras.Count > 0 && numeros.Count > 0)
+                {
+                    rua = string.Join(" ", palavras);
+                    numero = string.Join(" ", numeros);
+                    return new string[] { rua, numero }; // retorna o resultado da analise;
+                }
+                else
+                {
+                    return new string[] { "Não foi possível REGISTRAR este endereço", null };
+                }
+                
+            }
+            else
+            {
+                return new string[] { "Não foi possível REGISTRAR este endereço", null };
+            }
+        }
+
+
+
+        static bool Contemnumero(string palavraatual)
+        {
+            return palavraatual.Any(char.IsDigit);
+        }
+        static string Buscaindicador(string[] palavraatual)
+        {
+            List<string> termos = new List<string> { "Número", "número", "Numero", "numero", "No", "Nº" }; // termos para identificação de qual parte é o numero
+            string indicador = null;
+            foreach (var palavra in palavraatual)
+            {
+                if (termos.Any(termo => palavra.Contains(termo)))
+                {
+                    indicador = palavra;
+                }
+            }
+            return indicador;
+        }
+        static string Buscacomplemento(string[] palavraatual)
+        {
+            string complemento = null;
+            foreach(var termo in palavraatual)
+            {
+                if (termo.Length == 1 && !Contemnumero(termo))
+                {
+                    if (Contemnumero(palavraatual[Array.IndexOf(palavraatual, termo) - 1])) // checa se é um complemento baseado no elemento anterior
+                    {
+                        complemento = termo;
+                    }
+                }
+            }
+            return complemento;
+        }
+        static string Buscanumerosexed(string[] palavraatual)
+        {
+            string resnum = null;
+            List<string> numeros = new List<string>();
+            List<string> separanum = new List<string>();
+            foreach(var termo in palavraatual)
+            {
+                if(Contemnumero(termo))
+                {
+                    numeros.Add(termo);
+                }
+            }
+            if(numeros.Count > 1) // em caso de mais de um numero no endereço
+            {
+                string indicador = Buscaindicador(palavraatual); // checa se existe um indicador de numero
+                foreach (var termo in palavraatual)
+                {
+                    if(Contemnumero(termo) && indicador != palavraatual[Array.IndexOf(palavraatual, termo) - 1]) // checa se o termo anterior é um indicador
+                    {
+                        separanum.Add(termo);
+                    }
+                }
+            }
+            resnum = string.Join(" ", separanum.ToArray());
+            return resnum;
+        }
+
+
+
+        /*string rua = "", numero = "";
+            if(analise.Contains(" ")) // busca espaços no endereço para separar palavras
+            {
+                string[] separa = analise.Split(' ');
+                List<string> palavras = new List<string>();
+                List<string> numeros = new List<string>();
+
+                if (Contemnumero(analise)) // busca numeros entre as palavras separadas
+                {
+                    foreach (var termo in separa)
+                    {
+                        if(Contemnumero(termo))
+                        {
+                            numeros.Add(termo);
+                        }
+                        else
+                        {
+                            if(termo.Length == 1 && numeros.Count == 1)
+                            {
+                                if(Contemnumero(separa[Array.IndexOf(separa, termo) - 1])) // checa se é um complemento baseado no elemento anterior
+                                {
+                                    numeros.Add(termo);
+                                }
+                                else
+                                {
+                                    palavras.Add(termo);
+                                }
+                            }
+                            else
+                            {
+                                palavras.Add(termo);
+                            }
+                        }
+                    }
+                    if(numero.Length > 1) // checa se há mais de um item na lista numero registrado
+                    {
+                        if (Buscaindicador(analise)) // busca identificador de numero de endereço
+                        {
+                            foreach (var palavra in separa)
+                            {
+                                bool encontrou = false;
+                                if (Buscaindicador(palavra))
+                                {
+                                    encontrou = true;
+                                }
+                                if (!encontrou)
+                                {
+                                    palavras.Add(palavra);
+                                }
+                                else
+                                {
+                                    numeros.Add(palavra);
+                                }
+                            }
+                            rua = string.Join(" ", palavras);
+                            numero = string.Join(" ", numeros);
+                            return new string[] { rua, numero };
+                        }
+                        else
+                        {
+                            foreach (var termo in numeros)
+                            {
+                                if(termo.Length == 1 && !Contemnumero(termo)) // verifica se é um complemento
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    return new string[] { "Não foi possível REGISTRAR este endereço(erro 3)", null };
+                                }
+                            }
+                            rua = string.Join(" ", palavras);
+                            numero = string.Join(" ", numeros);
+                            return new string[] { rua, numero };
+                        }
+                    }
+                    else
+                    {
+                        rua = string.Join(" ", palavras);
+                        numero = string.Join(" ", numeros);
+                        return new string[] { rua, numero };
+                    }
+                }
+                else
+                {
+                    return new string[] { "Não foi possível REGISTRAR este endereço(erro 2)", null };
+                }
+            }
+            else
+            {
+                return new string[] { "Não foi possível REGISTRAR este endereço (Erro 1)", null };
+            }*/
+    }
+}
